b5b0b055c6d10433038c215e90961be0b7121b5a
ansarimohammed422 <ansarimohammed122@gmail.com>
2026-07-10 22:30:55 +0530

    Revert "Switch to AWQ quantized model to drastically reduce cold start disk I/O time"
    This reverts commit 4c6228cc4cd42c74565bf96699ec8f31edab0118.

--- handler.py
+++ handler.py
@@ -30,14 +30,14 @@ else:
 try:
     print("Loading Qwen2-VL Model into VRAM...")
     model = Qwen2VLForConditionalGeneration.from_pretrained(
-        "Qwen/Qwen2-VL-7B-Instruct-AWQ", 
+        "Qwen/Qwen2-VL-7B-Instruct", 
         torch_dtype=torch.bfloat16, 
         device_map="auto",
         low_cpu_mem_usage=True,
         cache_dir=CACHE_DIR
     )
     processor = AutoProcessor.from_pretrained(
-        "Qwen/Qwen2-VL-7B-Instruct-AWQ",
+        "Qwen/Qwen2-VL-7B-Instruct",
         cache_dir=CACHE_DIR
     )
     print("Model loaded successfully!")
